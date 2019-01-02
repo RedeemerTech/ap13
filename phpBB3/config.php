fb277a64fb5c2629ba7478880a01487d1afdf052
kayalvizhivn <kayalvizhi.pragaatheshwaran@redeemertech.co.uk>
2019-01-02 11:22:41 +0000

    updated
    updated

---
+++ phpBB3/config.php
@@ -0,0 +1,17 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'phpbb\\db\\driver\\mysqli';
+$dbhost = 'localhost';
+$dbport = '';
+$dbname = 'gracemat_phpb164';
+$dbuser = 'gracemat_phpb164';
+$dbpasswd = 'jS-5-I0p43';
+$table_prefix = 'phpbbsj_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+@define('PHPBB_ENVIRONMENT', 'production');
+// @define('DEBUG_CONTAINER', true);
